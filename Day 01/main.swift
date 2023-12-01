197e3eb3acb36f8b1c6852aef6a61ca6ae130f14
Julian Dunskus <julian.dunskus@gmail.com>
2023-12-01 18:29:39 +0100

    day 1

---
+++ Day 01/main.swift
@@ -0,0 +1,38 @@
+import Foundation
+import AoC_Helpers
+import SimpleParser
+
+let lines = input().lines()
+let part1Value = lines
+	.lazy
+	.map { "\($0.first(where: \.isNumber)!)\($0.last(where: \.isNumber)!)" }
+	.map { Int($0)! }
+	.sum()
+print(part1Value)
+
+let spellings = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+func digits(of line: some StringProtocol) -> [Int] {
+	var digits: [Int] = []
+	for start in line.indices {
+		let rest = line[start...]
+		if rest.first!.isNumber {
+			digits.append(.init(rest.prefix(1))!)
+		} else {
+			for (number, spelling) in spellings.enumerated() {
+				if rest.hasPrefix(spelling) {
+					digits.append(number)
+					break
+				}
+			}
+		}
+	}
+	return digits
+}
+
+let part2Value = lines
+	.lazy
+	.map(digits(of:))
+	.map { $0.first! * 10 + $0.last! }
+	.sum()
+print(part2Value) // 53846 too low
