12a2ac10843c04a9d60e0100032b4a363f6db6cf
Julian Dunskus <julian.dunskus@gmail.com>
2023-12-11 06:20:04 +0100

    day 11

---
+++ Day 11/main.swift
@@ -0,0 +1,32 @@
+import Foundation
+import AoC_Helpers
+
+let space = Matrix(input().lines())
+let galaxies: Array = space.indexed()
+	.lazy
+	.filter { $0.element == "#" }
+	.map(\.index)
+
+// prefix sums
+let emptyRows: Array = space.rows
+	.lazy
+	.map { $0.contains("#") ? 0 : 1 }
+	.reductions(0, +)
+let emptyCols: Array = space.columns()
+	.lazy
+	.map { $0.contains("#") ? 0 : 1 }
+	.reductions(0, +)
+
+func printDistances(atAge age: Int) {
+	func distanceBetween(_ a: Vector2, _ b: Vector2) -> Int {
+		let rowStretch = emptyRows[max(a.y, b.y)] - emptyRows[min(a.y, b.y)]
+		let colStretch = emptyCols[max(a.x, b.x)] - emptyCols[min(a.x, b.x)]
+		return a.distance(to: b) + (rowStretch + colStretch) * (age - 1)
+	}
+	
+	let distances = galaxies.pairwiseCombinations().lazy.map(distanceBetween)
+	print(distances.sum() / 2)
+}
+
+printDistances(atAge: 2)
+printDistances(atAge: 1_000_000)
